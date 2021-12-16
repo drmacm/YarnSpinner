b11332c12981fe07ca731a610184a15251cf905f
drmacm <drmac.mladen@gmail.com>
2021-12-16 11:43:06 +0100

    Update AssemblyInfo

--- YarnSpinner.Compiler/AssemblyInfo.cs
+++ YarnSpinner.Compiler/AssemblyInfo.cs
@@ -1,4 +1,6 @@
 using System.Reflection;
+using System.Runtime.CompilerServices;
 
 [assembly: AssemblyVersion("2.0")]
 [assembly: AssemblyInformationalVersion("2.0.0-beta2")]
+[assembly: InternalsVisibleTo("YarnSpinner.Framework.Tests")]
